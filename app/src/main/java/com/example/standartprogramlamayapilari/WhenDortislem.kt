17fe00a0b2b1a6a580fc72d4ca05794268148916
Vefa Can <vefa.can.97@gmail.com>
2022-06-23 16:00:12 +0300

    When

---
+++ app/src/main/java/com/example/standartprogramlamayapilari/WhenDortislem.kt
@@ -0,0 +1,31 @@
+package com.example.standartprogramlamayapilari
+
+import java.util.*
+
+fun main() {
+
+    val girdi = Scanner(System.`in`)
+
+    println("Toplama için (1)")
+    println("Çıkarma için (2)")
+    println("Çarpma için (3)")
+    println("Bölme için (4)")
+
+    val tercih = girdi.nextInt()
+
+
+    println("İlk sayıyı giriniz:")
+    val ilksayi = girdi.nextInt()
+
+    println("İkinci sayıyı giriniz:")
+    val ikincisayi = girdi.nextInt()
+
+
+    when (tercih){
+
+        1-> println(ilksayi + ikincisayi)
+        2-> println(ilksayi - ikincisayi)
+        3-> println(ilksayi * ikincisayi)
+        4-> println(ilksayi / ikincisayi)
+    }
+}
