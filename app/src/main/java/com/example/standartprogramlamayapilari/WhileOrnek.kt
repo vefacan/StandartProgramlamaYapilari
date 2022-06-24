8a4361051586a5f11a2f32de5c16bceda39b5767
Vefa Can <vefa.can.97@gmail.com>
2022-06-24 16:54:54 +0300

    Döngülerrrr

---
+++ app/src/main/java/com/example/standartprogramlamayapilari/WhileOrnek.kt
@@ -0,0 +1,28 @@
+package com.example.standartprogramlamayapilari
+
+import java.util.*
+
+fun main (){
+
+    val girdi = Scanner(System.`in`)
+
+    println("İşlenecek veri miktarını giriniz:")
+
+    var veri = girdi.nextInt()
+
+
+
+    while (veri > 0 ){
+
+        println("$veri. veri ")
+
+        veri = veri - 1
+
+
+    }
+
+
+
+
+
+}
